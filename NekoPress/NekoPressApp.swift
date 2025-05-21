3db9830e9660fe02590066dba0e8230a85e6935a
MakiforDevelop <makifordevelop@gmail.com>
2025-05-21 15:19:28 +0800

    Initial Commit

---
+++ NekoPress/NekoPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoPressApp.swift
+//  NekoPress
+//
+//  Created by 千葉牧人 on 2025/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NekoPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
